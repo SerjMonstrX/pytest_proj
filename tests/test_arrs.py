133c0fc79abab0b327978e5e923a05046c6f9e54
SerjMonstrX <SerjMonstrX@gmail.com>
2023-10-04 01:37:14 +0700

    Added function get_val in file dicts.py and 100% covered with tests with func test_get_val

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,4 @@
-from utils import arrs
+from utils import arrs, dicts
 
 
 def test_get():
@@ -12,3 +12,10 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
     assert arrs.my_slice([]) == []
 
+def test_get_val():
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs" ) == "mercurial"
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs", 'git' ) == "mercurial"
+    assert dicts.get_val({"vcsss": "mercurial"}, "vcs", ) == 'git'
+    assert dicts.get_val({"vcsss": "mercurial"}, "vcs", 'bazaar') == 'bazaar'
+    assert dicts.get_val({}, "vcs" ) == 'git'
+    assert dicts.get_val({}, '') == 'git'
